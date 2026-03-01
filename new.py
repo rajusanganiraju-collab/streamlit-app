11ea1e457da3cf5f590d5a8df3199a78cf2a37fe
rajusanganiraju-collab <raju.sanganiraju@gmail.com>
2026-03-01 16:16:21 +0530

    Update new.py

--- new.py
+++ new.py
@@ -985,7 +985,7 @@ if not df.empty:
 
     # SORTING LOGIC 
     # 🔥 PURE ONE SIDED LOGIC OVERRIDE 🔥
-    sort_col = "S"
+    sort_col = "Trend_Score" if watchlist_mode == "One Sided Moves 🚀" else "S"
     
     if sort_mode == "% Change Up 🟢": 
         df_stocks_display = df_filtered.sort_values(by="C", ascending=False)
